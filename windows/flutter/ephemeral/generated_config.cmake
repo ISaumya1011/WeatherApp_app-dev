2f25f243a05432a41c74352da5df2493735363e8
Saumya Gupta <142958342+ISaumya1011@users.noreply.github.com>
2024-08-26 16:21:19 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Club Societies\\MSC\\MSC Bootcamp 2024\\App_dev\\Project_WeatherApp\\weather_app_project" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Flutter\\flutter"
+  "PROJECT_DIR=D:\\Club Societies\\MSC\\MSC Bootcamp 2024\\App_dev\\Project_WeatherApp\\weather_app_project"
+  "FLUTTER_ROOT=C:\\Flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Club Societies\\MSC\\MSC Bootcamp 2024\\App_dev\\Project_WeatherApp\\weather_app_project\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Club Societies\\MSC\\MSC Bootcamp 2024\\App_dev\\Project_WeatherApp\\weather_app_project"
+  "FLUTTER_TARGET=D:\\Club Societies\\MSC\\MSC Bootcamp 2024\\App_dev\\Project_WeatherApp\\weather_app_project\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Club Societies\\MSC\\MSC Bootcamp 2024\\App_dev\\Project_WeatherApp\\weather_app_project\\.dart_tool\\package_config.json"
+)
